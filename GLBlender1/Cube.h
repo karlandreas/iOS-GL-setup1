847b13087d1a613c6ad590d7a027ca06f237a808
Karl Andreas <kajohansen.1@gmail.com>
2014-11-30 13:19:20 +0100

    Part One Done, renders model and texture

---
+++ GLBlender1/Cube.h
@@ -0,0 +1,17 @@
+//
+//  Cube.h
+//  GLBlender1
+//
+//  Created by Super User on 30.11.14.
+//  Copyright (c) 2014 Super User. All rights reserved.
+//
+
+#ifndef GLBlender1_Cube_h
+#define GLBlender1_Cube_h
+
+const int cubeVertices;
+const float cubePositions[108];
+const float cubeTexels[72];
+const float cubeNormals[108];
+
+#endif
--- GLBlender1/ViewController.m
+++ GLBlender1/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "Cube.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    float rotate;
+}
+
+@property (strong, nonatomic) GLKBaseEffect *effect;
 
 @end
 
@@ -17,11 +22,100 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Set up context
+    EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:context];
+    
+    // Set up view
+    GLKView *glkview = (GLKView *)self.view;
+    glkview.context = context;
+    
+    // OpenGL ES Settings
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glEnable(GL_CULL_FACE);
+    
+    // Create effect
+    [self createEffect];
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Prepare effect
+    [self.effect prepareToDraw];
+    
+    // Set matrices
+    [self setMatrices];
+    
+    // Positions
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 0, cubePositions);
+    
+    // Normals
+    glEnableVertexAttribArray(GLKVertexAttribNormal);
+    glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 0, cubeNormals);
+    
+    // Texels
+    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 0, cubeTexels);
+    
+    // Draw Model
+    glDrawArrays(GL_TRIANGLES, 0, cubeVertices);
+}
+
+- (void)createEffect {
+    // Initialize
+    self.effect = [[GLKBaseEffect alloc] init];
+    
+    // Light
+    self.effect.light0.enabled = GL_TRUE;
+    self.effect.light0.position = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
+    self.effect.lightingType = GLKLightingTypePerPixel;
+    
+    // Texture
+    NSDictionary *options = @{ GLKTextureLoaderOriginBottomLeft: @YES };
+    NSError *error;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"cube.png" ofType:nil];
+    GLKTextureInfo *texture = [GLKTextureLoader textureWithContentsOfFile:path options:options error:&error];
+    
+    if(texture == nil)
+        NSLog(@"Error loading file: %@", [error localizedDescription]);
+    
+    self.effect.texture2d0.name = texture.name;
+    self.effect.texture2d0.enabled = true;
+}
+
+- (void)setMatrices {
+    
+    // Projection Matrix
+    const GLfloat aspectRatio = (GLfloat)(self.view.bounds.size.width) / (GLfloat)(self.view.bounds.size.height);
+    const GLfloat fieldView = GLKMathDegreesToRadians(90.0f);
+    const GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(fieldView, aspectRatio, 0.1f, 10.0f);
+    
+    self.effect.transform.projectionMatrix = projectionMatrix;
+    
+    // ModelView Matrix
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Identity;
+    modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, 0.0f, 0.0f, -5.0f);
+    modelViewMatrix = GLKMatrix4RotateX(modelViewMatrix, GLKMathDegreesToRadians(45.0f));
+     // ModelView Matrix rotation
+    modelViewMatrix = GLKMatrix4RotateY(modelViewMatrix, GLKMathDegreesToRadians(rotate));
+    modelViewMatrix = GLKMatrix4RotateZ(modelViewMatrix, GLKMathDegreesToRadians(rotate));
+    
+    self.effect.transform.modelviewMatrix = modelViewMatrix;
 }
 
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)update
+{
+    rotate += 1.0f;
 }
 
 @end
